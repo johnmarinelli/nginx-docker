320521ba803c179c12c76d6fd5f357d2f57a6fec
john marinelli <jonmarinelli@hotmail.com>
2015-12-23 13:44:57 -0800

    remove node stuff

--- start.sh
+++ start.sh
@@ -5,6 +5,6 @@ rm -rf nginx-node-docker-load-balancer
 git clone https://github.com/johnmarinelli/nginx-node-docker-load-balancer
 cd nginx-node-docker-load-balancer
 npm install
-nodejs .
+#nodejs .
 
 service nginx start
